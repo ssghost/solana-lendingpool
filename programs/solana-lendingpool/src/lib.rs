fbc281ae7406027ec2d44d16ab9daaf184575333
ssghost <ssprof0@gmail.com>
2025-12-29 20:03:16 +0800

    mod lib-rs

--- programs/solana-lendingpool/src/lib.rs
+++ programs/solana-lendingpool/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{self, Mint, Token, TokenAccount, Transfer};
 
 declare_id!("Bkkm6zdAUkacHwn5ZiwFjJRSomnbhnszg7UTDf32Dqg4");
 
@@ -6,11 +7,141 @@ declare_id!("Bkkm6zdAUkacHwn5ZiwFjJRSomnbhnszg7UTDf32Dqg4");
 pub mod solana_lendingpool {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn init_bank(ctx: Context<InitBank>, liquidation_threshold: u64, max_ltv: u64) -> Result<()> {
+        let bank = &mut ctx.accounts.bank;
+
+        bank.authority = ctx.accounts.signer.key(); 
+        bank.mint = ctx.accounts.mint.key();
+        bank.bank_token_account = ctx.accounts.bank_token_account.key();
+
+        bank.total_deposits = 0;
+        bank.total_borrowed = 0;
+        bank.liquidation_threshold = liquidation_threshold; 
+        bank.max_ltv = max_ltv;
+
+        msg!("Bank initialized! Authority: {}", bank.authority);
+        Ok(())
+    }
+
+    pub fn deposit(ctx: Context<Deposit>, amount: u64) -> Result<()> {
+        let bank = &mut ctx.accounts.bank;
+        let user_account = &mut ctx.accounts.user_account;
+        let cpi_accounts = Transfer {
+            from: ctx.accounts.user_token_account.to_account_info(),
+            to: ctx.accounts.bank_token_account.to_account_info(),
+            authority: ctx.accounts.signer.to_account_info(),
+        };
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+        
+        token::transfer(cpi_ctx, amount)?;
+        bank.total_deposits += amount;
+        user_account.deposit_amount += amount;
+        
+        if user_account.owner == Pubkey::default() {
+            user_account.owner = ctx.accounts.signer.key();
+        }
+
+        msg!("Deposit successful! Amount: {}", amount);
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitBank<'info> {
+    #[account(
+        init,                   
+        payer = signer,         
+        space = 8 + 32 + 8 + 8, 
+        seeds = [b"bank", mint.key().as_ref()],      
+        bump                    
+    )]
+    pub bank: Account<'info, Bank>,
+
+    pub mint: Account<'info, Mint>,
+
+    #[account(
+        init,
+        payer = signer,
+        token::mint = mint,
+        token::authority = bank,
+        seeds = [b"treasury", mint.key().as_ref()],
+        bump
+    )]
+    pub bank_token_account: Account<'info, TokenAccount>,
+
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub rent: Sysvar<'info, Rent>
+}
+
+#[derive(Accounts)]
+pub struct Deposit<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [b"bank", mint.key().as_ref()],
+        bump
+    )]
+    pub bank: Account<'info, Bank>,
+
+    #[account(
+        mut,
+        seeds = [b"treasury", mint.key().as_ref()],
+        bump
+    )]
+    pub bank_token_account: Account<'info, TokenAccount>,
+
+    #[account(mut)]
+    pub mint: Account<'info, Mint>,
+
+    #[account(
+        mut,
+        constraint = user_token_account.owner == signer.key(),
+        constraint = user_token_account.mint == mint.key()
+    )]
+    pub user_token_account: Account<'info, TokenAccount>,
+
+    #[account(
+        init_if_needed, 
+        payer = signer,
+        space = 8 + UserAccount::INIT_SPACE,
+        seeds = [b"user", signer.key().as_ref()],
+        bump
+    )]
+    pub user_account: Account<'info, UserAccount>,
+
+    pub token_program: Program<'info, Token>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Bank {
+    pub authority: Pubkey,
+    pub total_deposits: u64, 
+    pub total_borrowed: u64, 
+    pub mint: Pubkey,
+    pub bank_token_account: Pubkey,
+    pub liquidation_threshold: u64, 
+    pub max_ltv: u64
+}
+
+impl Bank {
+    pub const INIT_SPACE: usize = 32 + 32 + 32 + 8 + 8 + 8 + 8;
+}
+
+#[account]
+pub struct UserAccount {
+    pub owner: Pubkey,
+    pub deposit_amount: u64,
+    pub borrowed_amount: u64,
+    pub bump: u8,
+}
+
+impl UserAccount {
+    pub const INIT_SPACE: usize = 32 + 8 + 8;
+}
--- tests/solana-lendingpool.ts
+++ tests/solana-lendingpool.ts
@@ -1,16 +1,34 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { SolanaLendingpool } from "../target/types/solana_lendingpool";
+import { assert } from "chai";
 
-describe("solana-lendingpool", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+describe("solana_lendingpool", () => {
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
-  const program = anchor.workspace.solanaLendingpool as Program<SolanaLendingpool>;
+  const program = anchor.workspace.SolanaLendingpool as Program<SolanaLendingpool>;
+  let bankPda: anchor.web3.PublicKey;
+  
+  it("Bank Initialized!", async () => {
+    [bankPda] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("bank")],
+      program.programId
+    );
+    const tx = await program.methods
+      .initBank()
+      .accounts({
+        bank: bankPda,
+        signer: provider.wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .rpc();
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
     console.log("Your transaction signature", tx);
+    const bankAccount = await program.account.bank.fetch(bankPda);
+    console.log("Bank Authority:", bankAccount.authority.toBase58());
+    console.log("Total Deposits:", bankAccount.totalDeposits.toString());
+    assert.ok(bankAccount.totalDeposits.eq(new anchor.BN(0)), "Deposits should be 0");
+    assert.ok(bankAccount.authority.equals(provider.wallet.publicKey), "Authority should be me");
   });
 });
